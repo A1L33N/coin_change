21ccdb27cec72a8558dd580b0071d38825fc36c6
Aileen Cacayorin & Christoph Pirringer <student@epicodus.com>
2015-08-06 11:24:13 -0700

    Returns change for quarters, dimes, and nickels

--- lib/coin_change.rb
+++ lib/coin_change.rb
@@ -1,18 +1,55 @@
 class Fixnum
   define_method(:coin_change) do
-    input = self
-    remaining_change = 0
+    remaining_change = self
     quarter = 0
-    change = ""
+    dime = 0
+    nickel = 0
+    change=""
 
-    if input.>=(25)
-      quarter = (input./(25)).floor()
-      remaining_change = input.-(quarter.*(25))
+    until remaining_change.<(5)
+      if remaining_change.>25
+        quarter = (remaining_change./(25)).floor()
+        remaining_change = remaining_change.-(quarter.*(25))
+        quarter = quarter.to_s()
+
+        change = change.concat(quarter.concat(" Quarter"))
+        if quarter.to_f().>1
+          change.concat("s")
+        end
+      end
+
+      remaining_change
+      change
+
+      if remaining_change.>=(10)
+        dime = (remaining_change./(10)).floor()
+        remaining_change = remaining_change.-(dime.*(10))
+        dime = dime.to_s()
+
+        change = change.concat(", ").concat(dime.concat(" Dime"))
+        if dime.to_f().>1
+          change.concat("s")
+        end
+      end
+
+      remaining_change
+      change
+
+      if remaining_change.>=(5)
+        nickel = (remaining_change./(5)).floor()
+        remaining_change = remaining_change.-(nickel.*(5))
+        nickel = nickel.to_s()
+
+        change = change.concat(", ").concat(nickel.concat(" Nickel"))
+#binding.pry()
+        if nickel.to_f().>1
+          change.concat("s")
+        end
+      end
+
     end
-    change = quarter.to_s().concat(" Quarter")
-    if quarter.>1
-      change.concat("s")
-    end
+
     change
-  end
+
+    end
 end
--- spec/coin_change_spec.rb
+++ spec/coin_change_spec.rb
@@ -1,12 +1,21 @@
 require('rspec')
 require('coin_change')
+require('pry')
 
 describe('Fixnum#coin_change') do
   it("returns one quarter of change for 25 cents of input") do
-    expect((25).coin_change()).to(eq("1 Quarter"))
+    expect((26).coin_change()).to(eq("1 Quarter"))
   end
 
   it("returns plural of quarters of change for 50 cents of input") do
     expect((50).coin_change()).to(eq("2 Quarters"))
+  end
+
+  it("returns plural of quarters and dimes of change for 95 cents of input") do
+    expect((95).coin_change()).to(eq("3 Quarters, 2 Dimes"))
+  end
+
+  it("returns plural of quarters and dimes and nickels of change for 65 cents of input") do
+    expect((65).coin_change()).to(eq("2 Quarters, 1 Dime, 1 Nickel"))
   end
 end
